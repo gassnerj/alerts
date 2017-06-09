b6e57cc67a121cca93f26f4b9919ce8f487f87b8
Jason Gassner <byrd8378160@outlook.com>
2017-06-08 23:08:40 -0500

    Added ability to click on each alert and have a popup display.

--- alert.js
+++ alert.js
@@ -1,8 +1,8 @@
 var jsonAlerts;
+var url = "https://api.weather.gov/alerts?active=1&zone_type=land&severity=severe";
 
-function loadDoc() {
+function loadDoc(url) {
   var xhttp = new XMLHttpRequest();
-  var url = "https://api.weather.gov/alerts?active=1&zone_type=land&severity=severe";
   var x;
   var alertObj;
   var i;
@@ -10,36 +10,55 @@ function loadDoc() {
 
   var table = document.getElementById("alerts");
 
-  
 
 
-  xhttp.onreadystatechange = function() {
+
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-     	alertObj = JSON.parse(this.responseText);
+      alertObj = JSON.parse(this.responseText);
 
-        document.getElementById("title").innerHTML = alertObj['title'];
+      document.getElementById("title").innerHTML = alertObj['title'];
 
 
-     	for (i=0; i < alertObj.features.length; i++) {
-            x = alertObj.features[i];
-            var alertID = x['properties'].id;
-            var activeID = document.getElementById(alertID);
-            if (!activeID) {
-              n++;
-              var row = table.insertRow(n+1);
-              row.setAttribute("id", x['properties'].id);
+      for (i = 0; i < alertObj.features.length; i++) {
+        x = alertObj.features[i];
+        var alertID = x['properties'].id;
+        var activeID = document.getElementById(alertID);
+        if (!activeID) {
+          n++;
+          var row = table.insertRow(n + 1);
+          row.setAttribute("id", x['properties'].id);
 
-              var cell1 = row.insertCell(0);
-              var cell2 = row.insertCell(1);
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
 
-              cell1.innerHTML = x['properties'].effective;
-              cell2.innerHTML = x['properties'].headline;
-            } else {
-                row.deleteRow(x);
-                alert("Warning " + x['properties'].id + " expired.");
-                
-              }
-     	}
+          var aID = "http://api.weather.gov/alerts/" + x['properties'].id;
+
+          row.setAttribute("alt", x['properties'].areaDesc);
+          cell1.innerHTML = "<a href=\"#\" onclick=loadDetails(\""+ x['properties'].id + "\")>View Alert</a>";
+          cell1.setAttribute("class", "my_popup_open");
+          cell2.innerHTML = x['properties'].headline;
+          switch(x['properties'].event) {
+            case "Severe Thunderstorm Warning":
+              row.setAttribute("style", "background-color:yellow");
+              break;
+            case "Tornado Warning":
+              row.setAttribute("style", "background-color:red");
+              break;
+            case "Flood Warning":
+              row.setAttribute("style", "background-color:green");
+              break;
+            case "Severe Thunderstorm Watch":
+              row.setAttribute("style", "color:yellow;background-color:black");
+              break;
+          }
+
+        } else {
+          row.deleteRow(x);
+          alert("Warning " + x['properties'].id + " expired.");
+
+        }
+      }
     }
   };
   xhttp.open("GET", url, true);
@@ -48,25 +67,16 @@ function loadDoc() {
 
 
 $(document).ready(function () {
-  $('#get-data').click(function () {
-    var showData = $('#show-data');
-
-    $.getJSON('https://api.weather.gov/alerts?active=1&zone_type=land&severity=severe', function (data) {
-      console.log(data);
-
-      var items = data.items.map(function (item) {
-        return item.key + ': ' + item.value;
-      });
-
-      showData.empty();
-
-      if (items.length) {
-        var content = '<li>' + items.join('</li><li>') + '</li>';
-        var list = $('<ul />').html(content);
-        showData.append(list);
-      }
-    });
-
-    showData.text('Loading the JSON file.');
-  });
+  loadDoc(url);
+  setInterval("location.reload(true)", 60000);
+  $('#my_popup').popup();
 });
+
+function loadDetails(alertID) {
+  alertID = "http://api.weather.gov/alerts/" + alertID;
+  
+  document.getElementById("alertID").innerHTML = alertID;
+
+
+  //alert(alertID);
+}
